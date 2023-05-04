06f25bbbd95580f0a2dc5ed62cae3fcae3a4b233
Maite <martinezf.maite@gmail.com>
2023-05-04 14:10:21 -0300

    Fixes password and email field states, sums up all the cases in one object

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -5,19 +5,72 @@ import styles from './login.module.scss';
 import { ReactComponent as EyeSVG } from '../../assets/icons/eye.svg';
 import { ReactComponent as xSVG } from '../../assets/icons/x.svg';
 
+type EmailType = {
+  inputValueEmail: string,
+  helperTextEmail: string,
+  fieldStatusEmail: TextFieldStatus,
+};
+
+const initEmailState = {
+  inputValueEmail: '',
+  helperTextEmail: '',
+  fieldStatusEmail: TextFieldStatus.default,
+};
+
+type PassType = {
+  inputValuePass: string,
+  helperTextPass: string,
+  fieldStatusPass: TextFieldStatus,
+};
+
+const initPassState = {
+  inputValuePass: '',
+  helperTextPass: '',
+  fieldStatusPass: TextFieldStatus.default,
+};
+
 export const Login = () => {
   const mailValidation = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  const [inputValue, setInputValue] = useState('');
-  const [helperText, setHelperText] = useState('');
-  const [fieldStatus, setFieldStatus] = useState(TextFieldStatus.default);
+  const passValidation = /^(?=.*[\W_])[a-zA-Z0-9\W_]+$/;
+
+  const [emailState, setEmailState] = useState<EmailType>(initEmailState);
+  const [passState, setPassState] = useState<PassType>(initPassState);
 
   useEffect(() => {
-    if (!(inputValue.match(mailValidation)) && (inputValue !== '')) {
-      setHelperText('Debe ser un email válido');
-      setFieldStatus(TextFieldStatus.error);
+    if (!(emailState.inputValueEmail.match(mailValidation)) && (emailState.inputValueEmail !== '')) {
+      setEmailState({
+        ...emailState,
+        helperTextEmail: 'Debe ser un email válido',
+        fieldStatusEmail: TextFieldStatus.error,
+      });
     } else {
-      setHelperText('');
-      setFieldStatus(TextFieldStatus.default);
+      setEmailState({
+        ...emailState,
+        helperTextEmail: '',
+        fieldStatusEmail: TextFieldStatus.default,
+      });
+    }
+  });
+
+  useEffect(() => {
+    if ((passState.inputValuePass.length < 8) && (passState.inputValuePass !== '')) {
+      setPassState({
+        ...passState,
+        helperTextPass: 'La contraseña debe ser más larga que 8 caracteres',
+        fieldStatusPass: TextFieldStatus.error,
+      });
+    } else if ((!passState.inputValuePass.match(passValidation)) && ((passState.inputValuePass !== ''))) {
+      setPassState({
+        ...passState,
+        helperTextPass: 'La contraseña debe tener al menos un caracter especial y un caracter alfanumerico',
+        fieldStatusPass: TextFieldStatus.error,
+      });
+    } else {
+      setPassState({
+        ...passState,
+        helperTextPass: '',
+        fieldStatusPass: TextFieldStatus.default,
+      });
     }
   });
 
@@ -31,27 +84,41 @@ export const Login = () => {
           <h1 className={styles.header1}>Iniciar sesión</h1>
           <section className={styles.textField}>
             <TextField
-              status={fieldStatus}
+              status={emailState.fieldStatusEmail}
               name="email"
-              onChange={(e) => setInputValue(e.target.value)}
+              onChange={(e) => setEmailState({
+                ...emailState,
+                inputValueEmail: e.target.value,
+              })}
               label="Usuario"
               type="email"
-              helperText={helperText}
+              helperText={emailState.helperTextEmail}
               helperIcon={xSVG}
             />
           </section>
 
           <section className={styles.textField}>
             <TextField
+              status={passState.fieldStatusPass}
               name="password"
-              onChange={() => { }}
+              onChange={(e) => setPassState({
+                ...passState,
+                inputValuePass: e.target.value,
+              })}
               label="Contraseña"
               type="password"
               rightIcon={EyeSVG}
+              helperText={passState.helperTextPass}
+              helperIcon={xSVG}
             />
           </section>
 
-          <Button className={`${styles.btnIngresar} ${styles.textField}`}>Ingresar</Button>
+          <Button
+            className={`${styles.btnIngresar} ${styles.textField}`}
+            disabled={(emailState.inputValueEmail === '') || (passState.inputValuePass === '')}
+          >
+            Ingresar
+          </Button>
 
           <a className={`${styles.textField} ${styles.link}`} href="http://www.google.com">¿Olvidaste tu contraseña?</a>
 
