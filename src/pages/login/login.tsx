c3c4cad18e55a581983f42b6168520b1548b019d
Maite <martinezf.maite@gmail.com>
2023-05-05 16:21:58 -0300

    Corrects format in files

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,30 +1,23 @@
-import { useState } from 'react';
-
 import { Button } from 'common/button';
 import { TextField } from 'common/text-field';
 import styles from './login.module.scss';
 import { ReactComponent as EyeSVG } from '../../assets/icons/eye.svg';
 
-export const Login = () => {
+export const Login = () => (
+  <div className={styles.container}>
 
-  return (
-    <>
-      <div className={styles.container}>
-
-        <div className={styles.formDivision}>
-          <div className={styles.formContainer}>
-            <img src="src/assets/icons/barco.svg" alt="Imagen de un barco" className={styles.icono} />
-            <h1 className={styles.header1}>Iniciar sesión</h1>
-            <TextField className={styles.textField} name={"email"} onChange={() => { }} label='Usuario' type='email'></TextField>
-            <TextField className={styles.textField} name={"password"} onChange={() => { }} label='Contraseña' type='password' rightIcon={EyeSVG} ></TextField>
-            <Button className={`${styles.btnIngresar} ${styles.textField}`}>Ingresar</Button>
-            <a className={`${styles.textField} ${styles.link}`} href="http://www.google.com">¿Olvidaste tu contraseña?</a>
-          </div>
-        </div>
-
-        <div className={styles.imgDivision} />
-
+    <div className={styles.formDivision}>
+      <div className={styles.formContainer}>
+        <img src="src/assets/icons/barco.svg" alt="Imagen de un barco" className={styles.icono} />
+        <h1 className={styles.header1}>Iniciar sesión</h1>
+        <TextField className={styles.textField} name="email" onChange={() => { }} label="Usuario" type="email" />
+        <TextField className={styles.textField} name="password" onChange={() => { }} label="Contraseña" type="password" rightIcon={EyeSVG} />
+        <Button className={`${styles.btnIngresar} ${styles.textField}`}>Ingresar</Button>
+        <a className={`${styles.textField} ${styles.link}`} href="http://www.google.com">¿Olvidaste tu contraseña?</a>
       </div>
-    </>
-  )
-};
+    </div>
+
+    <div className={styles.imgDivision} />
+
+  </div>
+);
