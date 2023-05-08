64905a69836ae04f1066e7c24d1ad234bd9569e8
Maite <martinezf.maite@gmail.com>
2023-05-08 12:10:12 -0300

    Fixed for the PR

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Button } from 'common/button';
 import { TextField, TextFieldStatus } from 'common/text-field';
-import styles from './login.module.scss';
 import { ReactComponent as EyeSVG } from '../../assets/icons/eye.svg';
 import { ReactComponent as ClosedEyeSVG } from '../../assets/icons/closed-eye.svg';
 import { ReactComponent as xSVG } from '../../assets/icons/x.svg';
 import { mailFormat, passwordFormat } from '../../helpers/utils.js';
+import styles from './login.module.scss';
 
 type EmailType = {
   inputValueEmail: string,
@@ -36,8 +36,8 @@ export const Login = () => {
   const [passState, setPassState] = useState<PassType>(initPassState);
   const [passHidden, setPassHidden] = useState('password');
 
-  useEffect(() => {
-    if (!emailState.inputValueEmail.match(mailFormat) && emailState.inputValueEmail !== '') {
+  const checkEmailValidation = (e: React.FocusEvent<HTMLInputElement>) => {
+    if (!e.target.value.match(mailFormat) && e.target.value !== '') {
       setEmailState({
         ...emailState,
         helperTextEmail: 'Debe ser un email válido',
@@ -50,16 +50,16 @@ export const Login = () => {
         fieldStatusEmail: TextFieldStatus.default,
       });
     }
-  }, [emailState.inputValueEmail]);
+  };
 
-  useEffect(() => {
-    if (passState.inputValuePass.length < 8 && passState.inputValuePass !== '') {
+  const checkPassValidation = (e: React.FocusEvent<HTMLInputElement>) => {
+    if (e.target.value.length < 8 && e.target.value !== '') {
       setPassState({
         ...passState,
         helperTextPass: 'La contraseña debe ser más larga que 8 caracteres',
         fieldStatusPass: TextFieldStatus.error,
       });
-    } else if (!passState.inputValuePass.match(passwordFormat) && passState.inputValuePass !== '') {
+    } else if (!e.target.value.match(passwordFormat) && e.target.value !== '') {
       setPassState({
         ...passState,
         helperTextPass: 'La contraseña debe tener al menos un caracter especial y un caracter alfanumerico',
@@ -72,7 +72,7 @@ export const Login = () => {
         fieldStatusPass: TextFieldStatus.default,
       });
     }
-  }, [passState.inputValuePass]);
+  };
 
   const unmaskPass = function unmask() {
     if (passHidden === 'text') {
@@ -89,44 +89,43 @@ export const Login = () => {
           <img src="src/assets/icons/barco.svg" alt="Imagen de un barco" className={styles.boatIcon} />
 
           <h1 className={styles.header1}>Iniciar sesión</h1>
-          <section className={styles.textField}>
-            <TextField
-              status={emailState.fieldStatusEmail}
-              name="email"
-              onBlur={(e) => setEmailState({
-                ...emailState,
-                inputValueEmail: e.target.value,
-              })}
-              label="Usuario"
-              type="email"
-              helperText={emailState.helperTextEmail}
-              helperIcon={xSVG}
-            />
-          </section>
+          <TextField
+            status={emailState.fieldStatusEmail}
+            name="email"
+            onChange={(e) => setEmailState({
+              ...emailState,
+              inputValueEmail: e.target.value,
+            })}
+            onBlur={(e) => checkEmailValidation(e)}
+            label="Usuario"
+            type="email"
+            helperText={emailState.helperTextEmail}
+            helperIcon={xSVG}
+            errorMsg
+          />
 
-          <section className={styles.textField}>
-            <TextField
-              status={passState.fieldStatusPass}
-              name="password"
-              onBlur={(e) => setPassState({
-                ...passState,
-                inputValuePass: e.target.value,
-              })}
-              label="Contraseña"
-              type={passHidden}
-              rightIcon={unmaskPass()}
-              onRightIconClick={() => {
-                if (passHidden === 'text') {
-                  setPassHidden('password');
-                } else {
-                  setPassHidden('text');
-                }
-              }}
-              helperText={passState.helperTextPass}
-              helperIcon={xSVG}
-              errorMsg
-            />
-          </section>
+          <TextField
+            status={passState.fieldStatusPass}
+            name="password"
+            onChange={(e) => setPassState({
+              ...passState,
+              inputValuePass: e.target.value,
+            })}
+            onBlur={(e) => checkPassValidation(e)}
+            label="Contraseña"
+            type={passHidden}
+            rightIcon={unmaskPass()}
+            onRightIconClick={() => {
+              if (passHidden === 'text') {
+                setPassHidden('password');
+              } else {
+                setPassHidden('text');
+              }
+            }}
+            helperText={passState.helperTextPass}
+            helperIcon={xSVG}
+            errorMsg
+          />
 
           <Button
             className={`${styles.btnIngresar} ${styles.textField}`}
